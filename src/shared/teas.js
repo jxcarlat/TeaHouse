1f52a875fca979d3c761906fb06023c5bc627b67
Josh Carlat <joshbusiness22@gmail.com>
2020-09-08 12:49:09 -0500

    Tea Descriptions Updated

--- src/shared/teas.js
+++ src/shared/teas.js
@@ -3,7 +3,7 @@ export const TEAS = [
         id: 0,
         name: 'Black Tea',
         image: 'assets/images/black-tea.jpg',
-        description: "Here's Tea",
+        description: "Black tea is a true tea that comes from the Camellia sinensis plant. Black tea leaves are allowed to fully oxidize before being processed and dried, which makes the leaves dark brown and gives the tea its signature flavor profile. Black teas tend to be bold and brisk, and they are often described as astringent.",
         comments: [
             {
                 id: 0,
@@ -46,7 +46,7 @@ export const TEAS = [
         id: 1,
         name: 'White Tea',
         image: 'assets/images/white-tea.jpg',
-        description: "Here's more tea",
+        description: "White tea is tea made from new growth buds and young leaves of the plant Camellia sinensis. The leaves are steamed or fried to inactivate oxidation, and then dried. For white tea, the little buds that form on the plant are covered with silver hairs that give the young leaves a white appearance.",
         comments: [
             {
                 id: 0,
@@ -89,7 +89,7 @@ export const TEAS = [
         id: 2,
         name: 'Green Tea',
         image: 'assets/images/green-tea.jpg',
-        description: "Here's your tea",
+        description: "Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas. Green tea originated in China, but its production and manufacture has spread to other countries in East Asia.",
         comments: [
             {
                 id: 0,
@@ -132,7 +132,7 @@ export const TEAS = [
         id: 3,
         name:'Ginseng Tea',
         image: 'assets/images/ginseng-tea.jpg',
-        description: "Some more tea for you",
+        description: "Ginseng tea is a traditional Korean tea made with ginseng. While it is called a tea, ginseng tea does not contain tea leaves. It is a herbal tea infusion made out of the ginseng plant's root.",
         comments: [
             {
                 id: 0,
@@ -175,7 +175,7 @@ export const TEAS = [
         id: 4,
         name: "Oolong Tea",
         image: "assets/images/oolong-tea.jpg",
-        description: "Here's the tea",
+        description: "Oolong tea is a type of tea that is sometimes called \"wulong\" or \"black dragon\" tea. After the tea leaves are picked, they are rolled and allowed to oxidize. Oxidation produces floral notes that characterize many oolongs.",
         comments: [
             {
                 id: 0,
