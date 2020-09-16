049bba590439c8a50562bb3c0a5b1e561c8e2cda
Josh Carlat <joshbusiness22@gmail.com>
2020-09-16 12:42:28 -0500

    Reverted back to normal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
+import { TEAS } from './shared/teas';
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-          <div className="App">
-              <Main />
+  constructor(props) {
+    super(props);
+    this.state = {
+      teas: TEAS
+    };
+  }
+
+      render(){
+        return(
+        <div className="App">
+          <Navbar dark color="primary">
+          <div className="container">
+              <NavbarBrand href="/">TeaHouse</NavbarBrand>
           </div>
-      </BrowserRouter>
+          </Navbar>
+          <Directory teas={this.state.teas} />
+        </div>
     );
   }
 }
