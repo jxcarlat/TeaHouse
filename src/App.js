9603f5283ca73b56b9c423a39f202742a7167ed0
Josh Carlat <joshbusiness22@gmail.com>
2020-09-16 11:36:04 -0500

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { TEAS } from './shared/teas';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      teas: TEAS
-    };
-  }
 
-    render(){
-      return (
-        <div className="App">
-          <Navbar dark color="primary">
-          <div className="container">
-              <NavbarBrand href="/">TeaHouse</NavbarBrand>
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+          <div className="App">
+              <Main />
           </div>
-          </Navbar>
-          <Directory teas={this.state.teas} />
-        </div>
-      );
-    };
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
