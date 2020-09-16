c8a5908333fc639aa6ac8b107e89e2ebf9fc0e57
Josh Carlat <joshbusiness22@gmail.com>
2020-09-16 11:33:44 -0500

    Fixed up comments

--- src/shared/teas.js
+++ src/shared/teas.js
@@ -3,215 +3,30 @@ export const TEAS = [
         id: 0,
         name: 'Black Tea',
         image: 'assets/images/black-tea.jpg',
-        description: "Black tea is a true tea that comes from the Camellia sinensis plant. Black tea leaves are allowed to fully oxidize before being processed and dried, which makes the leaves dark brown and gives the tea its signature flavor profile. Black teas tend to be bold and brisk, and they are often described as astringent.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "What a magnificent Tea!",
-                author: "Jeremy Jenkins",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "The tea was pretty good.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "I really loved the tea! Coming back again soon.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "A wonderful tea indeed!",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The tea is so damn good!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        description: "Black tea is a true tea that comes from the Camellia sinensis plant. Black tea leaves are allowed to fully oxidize before being processed and dried, which makes the leaves dark brown and gives the tea its signature flavor profile. Black teas tend to be bold and brisk, and they are often described as astringent."
     },
     {
         id: 1,
         name: 'White Tea',
         image: 'assets/images/white-tea.jpg',
-        description: "White tea is tea made from new growth buds and young leaves of the plant Camellia sinensis. The leaves are steamed or fried to inactivate oxidation, and then dried. For white tea, the little buds that form on the plant are covered with silver hairs that give the young leaves a white appearance.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "What a magnificent Tea!",
-                author: "Jeremy Jenkins",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "The tea was pretty good.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "I really loved the tea! Coming back again soon.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "A wonderful tea indeed!",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The tea is so damn good!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        description: "White tea is tea made from new growth buds and young leaves of the plant Camellia sinensis. The leaves are steamed or fried to inactivate oxidation, and then dried. For white tea, the little buds that form on the plant are covered with silver hairs that give the young leaves a white appearance."
     },
     {
         id: 2,
         name: 'Green Tea',
         image: 'assets/images/green-tea.jpg',
-        description: "Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas. Green tea originated in China, but its production and manufacture has spread to other countries in East Asia.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "What a magnificent Tea!",
-                author: "Jeremy Jenkins",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "The tea was pretty good.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "I really loved the tea! Coming back again soon.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "A wonderful tea indeed!",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The tea is so damn good!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        description: "Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas. Green tea originated in China, but its production and manufacture has spread to other countries in East Asia."
     },
     {
         id: 3,
         name:'Ginseng Tea',
         image: 'assets/images/ginseng-tea.jpg',
-        description: "Ginseng tea is a traditional Korean tea made with ginseng. While it is called a tea, ginseng tea does not contain tea leaves. It is a herbal tea infusion made out of the ginseng plant's root.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "What a magnificent Tea!",
-                author: "Jeremy Jenkins",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "The tea was pretty good.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "I really loved the tea! Coming back again soon.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "A wonderful tea indeed!",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The tea is so damn good!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        description: "Ginseng tea is a traditional Korean tea made with ginseng. While it is called a tea, ginseng tea does not contain tea leaves. It is a herbal tea infusion made out of the ginseng plant's root."
     },
     {
         id: 4,
         name: "Oolong Tea",
         image: "assets/images/oolong-tea.jpg",
-        description: "Oolong tea is a type of tea that is sometimes called \"wulong\" or \"black dragon\" tea. After the tea leaves are picked, they are rolled and allowed to oxidize. Oxidation produces floral notes that characterize many oolongs.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "What a magnificent Tea!",
-                author: "Jeremy Jenkins",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "The tea was pretty good.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "I really loved the tea! Coming back again soon.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "A wonderful tea indeed!",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The tea is so damn good!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        description: "Oolong tea is a type of tea that is sometimes called \"wulong\" or \"black dragon\" tea. After the tea leaves are picked, they are rolled and allowed to oxidize. Oxidation produces floral notes that characterize many oolongs."
     }
 ];
