59823882ef688c6d3a5e52c37ad6f2922e3341c0
Josh Carlat <joshbusiness22@gmail.com>
2020-09-18 17:51:46 -0500

    Up-to-date

--- src/components/TeaInfoComponent.js
+++ src/components/TeaInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderTea({tea}) {
     return (
@@ -18,6 +19,16 @@ function TeaInfo(props) {
     if (props.tea) {
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.tea.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.tea.name}</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     <RenderTea tea={props.tea} />
                 </div>
