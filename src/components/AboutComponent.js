91f0292281cceae30209f3c650dd3ccef2c5ef91
Josh Carlat <joshbusiness22@gmail.com>
2020-09-22 14:17:14 -0500

    Fixed responsive design for AboutComponent image

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -16,7 +16,7 @@ function About() {
                     <p>Do you seek a place to unwind and relax? A place where you can listen to chill music, look at beautiful scenery, all while drinking from a selection of gourmet teas? Well look no further, at Lofi Chai we service all of those needs and provide a little something extra for fans of the increasingly popular genre of music. We gladly represent ourselves as a venue for artists and host events in order to promote everything Lofi, we don’t care if it goes mainstream though, we prefer to stay underground. If you're a niche kind of customer then we’re your niche kind of place, come and check us out sometime and take a load off.</p>
                 </div>
                 <div className="col-sm-6">
-                    <img src="/assets/images/lofi.jpg" alt="Lofi Logo" />
+                    <img src="/assets/images/lofi.jpg" className="img-fluid" alt="Lofi Logo" />
                 </div>
             </div>
         </div>
