ce93f5befb222b80fe6b8e7b189e84525add4a7d
Josh Carlat <joshbusiness22@gmail.com>
2020-09-22 15:26:27 -0500

    Styling

--- src/components/EventComponent.js
+++ src/components/EventComponent.js
@@ -48,13 +48,13 @@ function Event(props) {
                     <img src="/assets/images/brunch-menu.jpg" alt="Brunch Menu" />
                 </div>
                 <div className="col-lg-4">
-                    <h3>Surf's Up Crepes and Chai</h3>
+                    <h4>Surf's Up Crepes and Chai</h4>
                     <p>When you participate in the HM Surf event you can try our limited special HM Surf blueberry crepes ($15) and get a cup of chai tea on the house. Surf's up my dude!</p>
                     <br />
-                    <h3>Ocha with some Mocha</h3>
+                    <h4>Ocha with some Mocha</h4>
                     <p>Come hang with us during the Ocha event and you can try the new Ocha Mocha Latte available for a limited time (or longer if you guys want, we won't be stingy about it).</p>
                     <br />
-                    <h3>Jinsang Jamboree</h3>
+                    <h4>Jinsang Jamboree</h4>
                     <p>Everything half-off. Yes we are being completely serious here, we just want you guys to come in and experience this one in a lifetime event with Jinsang, you can thank us later.</p>
                 </div>
             </div>
