00e40464d662215c90f7b7fb2724f389c85d29ae
Josh Carlat <joshbusiness22@gmail.com>
2020-09-21 19:28:07 -0500

    Fixed Footer responsive design

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,16 +6,16 @@ function Footer(props) {
         <footer className="site-footer">
             <div className="container">
                 <div className="row">             
-                    <div className="col-6 col-sm-3 offset-1">
+                    <div className="col-6 col-sm-3">
                         <h5 id="header">Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to='/home'>Home</Link></li>
                             <li><Link to='/directory'>Teas</Link></li>
                             <li><Link to='/aboutus'>About</Link></li>
                             <li><Link to='/contactus'>Contact</Link></li>
-                        </ul>
+                        </ul><br /><br /><br />
                     </div>
-                    <div className="col-6 col-sm-3 text-center offset-5">
+                    <div className="col-6 col-sm-3 text-center offset-sm-6">
                         <h5 id="header">Social</h5>
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
