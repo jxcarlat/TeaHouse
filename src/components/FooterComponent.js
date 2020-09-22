bf3e6ec0feb51cdfafa9cac6d2f7175af99ad069
Josh Carlat <joshbusiness22@gmail.com>
2020-09-21 21:29:07 -0500

    Fixed up Header and Footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -12,7 +12,7 @@ function Footer(props) {
                             <li><Link to='/home'>Home</Link></li>
                             <li><Link to='/directory'>Teas</Link></li>
                             <li><Link to='/aboutus'>About</Link></li>
-                            <li><Link to='/contactus'>Contact</Link></li>
+                            <li><Link to='/room'>Rooms</Link></li>
                             <li><Link to='/event'>Events</Link></li>
                         </ul><br /><br /><br />
                     </div>
